8bb08fc46b663f24488a5f0e7b490305bf51c2cf
OKUMURA-env <n.okumura@env-value.co.jp>
2023-07-03 11:56:53 +0900

    ヘッダーボタンの表示修正

--- resources/js/components/HeaderComponent.vue
+++ resources/js/components/HeaderComponent.vue
@@ -2,10 +2,10 @@
      <div class="container-fluid bg-dark mb-3">
          <div class="container">
              <nav class="navbar navbar-dark">
-                 <span class="navbar-brand mb-0 h1">Vue Laravel SPA</span>
+                 <span class="navbar-brand mb-0 h1">Vue Laravel3</span>
                  <div>
-                     <button class="btn btn-success">List</button>
-                     <button class="btn btn-success">ADD</button>
+                     <button class="btn btn-success">一覧</button>
+                     <button class="btn btn-success">新規登録</button>
                  </div>
              </nav>
          </div>
