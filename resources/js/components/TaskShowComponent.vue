559875cc28ce7f8bfcbdf2c23ab1cf56836c7487
OKUMURA-env <n.okumura@env-value.co.jp>
2023-07-04 19:37:13 +0900

    TaskShowComponent.vueの内容記述

---
+++ resources/js/components/TaskShowComponent.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="row">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-header">
+                    <h4>詳細</h4>
+                </div>
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-12 mb-2">
+                            <label>Title</label>
+                            <p>{{ task.title }}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        taskId: String,
+    },
+    data() {
+        return {
+            task: {
+                title: "",
+            },
+        };
+    },
+    mounted() {
+        this.showTask();
+    },
+    methods: {
+        showTask() {
+            axios
+                .get("/api/tasks/" + this.taskId)
+                .then((response) => {
+                    this.task = response.data;
+                })
+                .catch((error) => {
+                    console.log(error);
+                });
+        },
+    },
+};
+</script>
