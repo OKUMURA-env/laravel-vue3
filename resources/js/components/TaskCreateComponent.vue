7db5f388190d4d2110409b92592a1db37d8c84a7
OKUMURA-env <n.okumura@env-value.co.jp>
2023-07-04 13:56:14 +0900

    登録フォームを一旦タイトルのみに変更

--- resources/js/components/TaskCreateComponent.vue
+++ resources/js/components/TaskCreateComponent.vue
@@ -8,16 +8,6 @@
             <input type="text" class="col-sm-9 form-control" id="title" v-model="task.title"/>
           </div>
           {{ title }}
-          <div class="form-group row">
-            <label for="content" class="col-sm-3 col-form-label">タスク内容</label>
-            <input type="text" class="col-sm-9 form-control" id="content" v-model="task.content"/>
-          </div>
-          {{ content }}
-          <div class="form-group row">
-            <label for="person-in-charge" class="col-sm-3 col-form-label">担当者</label>
-            <input type="text" class="col-sm-9 form-control" id="person-in-charge" v-model="task.person_in_charge"/>
-            {{ person_in_charge }}
-          </div>
           <button type="submit" class="btn btn-primary">作成</button>
         </form>
       </div>
