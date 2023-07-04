f63a430829f928cf0d334f1369c6dc7e90a48297
OKUMURA-env <n.okumura@env-value.co.jp>
2023-07-04 15:27:14 +0900

    微修正

--- resources/js/components/TaskCreateComponent.vue
+++ resources/js/components/TaskCreateComponent.vue
@@ -1,34 +1,43 @@
 <template>
-  <div class="container">
-    <div class="row justify-content-center">
-      <div class="col-sm-6">
-        <form @submit.prevent="submit">
-          <div class="form-group row">
-            <label for="title" class="col-sm-3 col-form-label">タスク名</label>
-            <input type="text" class="col-sm-9 form-control" id="title" v-model="task.title"/>
-          </div>
-          {{ title }}
-          <button type="submit" class="btn btn-primary">作成</button>
-        </form>
-      </div>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-sm-6">
+                <form @submit.prevent="submit">
+                    <div class="form-group row">
+                        <label for="title" class="col-sm-3 col-form-label">タスク名</label>
+                        <input
+                            type="text"
+                            class="col-sm-9 form-control"
+                            id="title"
+                            v-model="task.title"
+                        />
+                    </div>
+                    {{ title }}
+                    <button type="submit" class="btn btn-primary">作成</button>
+                </form>
+            </div>
+        </div>
     </div>
-  </div>
 </template>
-   
+
 <script>
 export default {
-  data() {
-    return {
-      task: {}
-    }
-  },
-  methods: {
-    submit() {
-      axios.post('/api/tasks', this.task)
-        .then((res) => {
-          this.$router.push('/tasks')
-        })
-      }
-  },
+    data() {
+        return {
+            task: {},
+        };
+    },
+    methods: {
+        submit() {
+            axios
+                .post("/api/tasks", this.task)
+                .then((res) => {
+                    this.$router.push({ name: "tasks.list" });
+                })
+                .catch((error) => {
+                    console.log(error);
+                });
+        },
+    },
 };
 </script>
