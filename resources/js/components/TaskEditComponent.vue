d0c97bcee0bd6ce95e7a065db1fe6abf235c5d9e
OKUMURA-env <n.okumura@env-value.co.jp>
2023-07-05 08:41:32 +0900

    TaskEditComponent.vue作成

---
+++ resources/js/components/TaskEditComponent.vue
@@ -0,0 +1,49 @@
+<template>
+    <div>
+        <h3 class="text-center">Edit Article</h3>
+        <div class="row">
+            <div class="col-md-6">
+                <form>
+                    <div class="form-group">
+                        <label>Title</label>
+                        <input
+                            type="text"
+                            class="form-control"
+                            v-model="task.title"
+                        />
+                    </div>
+                    <button type="submit" class="btn btn-primary">
+                        Update
+                    </button>
+                </form>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        taskId: String,
+    },
+    data() {
+        return {
+            task: {},
+        };
+    },
+    methods: {
+        getTask() {
+            axios
+            .get("/api/tasks/" + this.taskId)
+            .then((res) => {
+                console.log(res);
+                this.task = res.data;
+            });
+        }
+        
+    },
+    mounted() {
+    this.getTask();
+  }
+};
+</script>
