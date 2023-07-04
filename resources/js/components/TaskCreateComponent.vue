d7f17a8ae169c99fefe7ae7d58d8f5787e60093b
OKUMURA-env <n.okumura@env-value.co.jp>
2023-07-04 11:54:49 +0900

    TaskCreateComponent.vueの中身追加

--- resources/js/components/TaskCreateComponent.vue
+++ resources/js/components/TaskCreateComponent.vue
@@ -1,10 +1,44 @@
 <template>
-    <div>
-        <h2>新規作成</h2>
-            <form>
-            <input type="text">
-            <button>追加</button>
-            </form>
-            
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-sm-6">
+        <form @submit.prevent="submit">
+          <div class="form-group row">
+            <label for="title" class="col-sm-3 col-form-label">タスク名</label>
+            <input type="text" class="col-sm-9 form-control" id="title" v-model="task.title"/>
+          </div>
+          {{ title }}
+          <div class="form-group row">
+            <label for="content" class="col-sm-3 col-form-label">タスク内容</label>
+            <input type="text" class="col-sm-9 form-control" id="content" v-model="task.content"/>
+          </div>
+          {{ content }}
+          <div class="form-group row">
+            <label for="person-in-charge" class="col-sm-3 col-form-label">担当者</label>
+            <input type="text" class="col-sm-9 form-control" id="person-in-charge" v-model="task.person_in_charge"/>
+            {{ person_in_charge }}
+          </div>
+          <button type="submit" class="btn btn-primary">作成</button>
+        </form>
+      </div>
     </div>
+  </div>
 </template>
+   
+<script>
+export default {
+  data() {
+    return {
+      task: {}
+    }
+  },
+  methods: {
+    submit() {
+      axios.post('/api/tasks', this.task)
+        .then((res) => {
+          this.$router.push('/tasks')
+        })
+      }
+  },
+};
+</script>
