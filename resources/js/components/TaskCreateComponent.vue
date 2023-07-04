d9b591f9f285995daaf94741f9f774a2f872939d
OKUMURA-env <n.okumura@env-value.co.jp>
2023-07-04 11:05:50 +0900

    TaskCreate追加

---
+++ resources/js/components/TaskCreateComponent.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        <h2>新規作成</h2>
+            <form>
+            <input type="text">
+            <button>追加</button>
+            </form>
+            
+    </div>
+</template>
