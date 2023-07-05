dbf0e53777d3105b69c10f91527cff23e0fa1b6d
OKUMURA-env <n.okumura@env-value.co.jp>
2023-07-05 14:43:52 +0900

    詳細ページに項目を追加

--- resources/js/components/TaskShowComponent.vue
+++ resources/js/components/TaskShowComponent.vue
@@ -8,8 +8,14 @@
                 <div class="card-body">
                     <div class="row">
                         <div class="col-12 mb-2">
-                            <label>Title</label>
+                            <label>タスク名</label>
                             <p>{{ task.title }}</p>
+                            <label>内容</label>
+                            <p>{{ task.description }}</p>
+                            <label>担当者</label>
+                            <p>{{ task.person_in_charge }}</p>
+                            <label>状況</label>
+                            <p>{{ task.status }}</p>
                         </div>
                     </div>
                 </div>
